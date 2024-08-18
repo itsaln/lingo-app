d8e6ab16940b8d4c21bde70ee3de120199fd3018
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-08-18 10:59:56 +0500

    init

---
+++ lib/admin.ts
@@ -0,0 +1,15 @@
+import { auth } from "@clerk/nextjs"
+
+const adminIds = [
+  "user_2kjUpdfk9e1aytWmFf2n4ozKXGf",
+];
+
+export const isAdmin = () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return false;
+  }
+
+  return adminIds.indexOf(userId) !== -1;
+};
